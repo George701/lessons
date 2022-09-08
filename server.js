d71a9771fcff4ed86af2539e72f7854a188423a7
George701 <chernikov.georgii@gmail.com>
2022-09-08 22:20:02 +0300

    add middleware and env variable

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express")
+require('dotenv').config()
 
 const app = express()
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
 
 app.get("/", (req, res) => {
   res.send("Welcome to the homepage")
@@ -10,4 +17,6 @@ app.get("/admin", (req, res) => {
   res.send("This is the top secret admin page")
 })
 
-app.listen(3000)
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
+})
